d835a38962a6211ca668bd29496dbc3fcc4fe0a6
unknown <baselsalah2053@gmail.com>
2023-03-24 23:13:19 +0200

    fixing typo in user migration up file

--- migrations/sqls/20230309133501-audit-table-up.sql
+++ migrations/sqls/20230309133501-audit-table-up.sql
@@ -1,10 +1,10 @@
 CREATE TABLE audit(
    audit_id SERIAL NOT NULL,
-   audit_action varchar(100) NOT NULL,
-   audit_data json,
-   audit_status integer,
-   audit_error json,
-   audit_by varchar(50) NOT NULL,
-   audit_on timestamp,
+   audit_action VARCHAR(100) NOT NULL,
+   audit_data JSON,
+   audit_status INTEGER,
+   audit_error JSON,
+   audit_by VARCHAR(50) NOT NULL,
+   audit_on TIMESTAMP,
    CONSTRAINT audit_pkey PRIMARY KEY (audit_id)
 );
