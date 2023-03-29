575f86b6f99e7044c1b1fcc666af2e5cfcad7bf8
unknown <baselsalah2053@gmail.com>
2023-03-29 22:51:24 +0200

    using imagekit for upload instead of filestack

--- dist/src/Error/error.type.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const ErrorType = {
-    API_ERROR: 'API_ERROR',
-    GENERAL_ERROR: 'GENERAL_ERROR',
-    SQL_INJECTION_ERROR: 'SQL_INJECTION_ERROR',
-};
-exports.default = ErrorType;
